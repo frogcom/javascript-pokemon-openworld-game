5f0381c94e86152a5e20b0442aaaa9738ed9a0d2
CasMeijer2005 <90309791+CasMeijer2005@users.noreply.github.com>
2022-06-23 20:30:29 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,344 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+// canvas.width = 1024 * 1.86;
+// canvas.height = 576 * 1.61;
+// canvas.width = outerWidth;
+// canvas.height = outerHeight;
+
+//dit stuk is voor de collisions
+const collisionsMap = [];
+for (let i = 0; i < collisions.length; i += 70) {
+  collisionsMap.push(collisions.slice(i, 70 + i));
+}
+
+const battleZonesMap = [];
+for (let i = 0; i < battleZonesData.length; i += 70) {
+  battleZonesMap.push(battleZonesData.slice(i, 70 + i));
+}
+
+const boundaries = [];
+const offset = {
+  x: -735,
+  y: -635,
+};
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      boundaries.push(
+        new boundary({
+          position: {
+            x: j * boundary.width + offset.x,
+            y: i * boundary.height + offset.y,
+          },
+        })
+      );
+  });
+});
+
+const battleZones = [];
+battleZonesMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      battleZones.push(
+        new boundary({
+          position: {
+            x: j * boundary.width + offset.x,
+            y: i * boundary.height + offset.y,
+          },
+        })
+      );
+  });
+});
+
+// hier word de player movement gedaan en de background inbeeld gebracht en de player
+const image = new Image();
+image.src = "assets/img/PelletTown.png";
+
+const foregroundImage = new Image();
+foregroundImage.src = "assets/img/foregroundObjects.png";
+
+//player movement images
+const playerDownImage = new Image();
+playerDownImage.src = "assets/img/playerDown.png";
+
+const playerUpImage = new Image();
+playerUpImage.src = "assets/img/playerUP.png";
+
+const playerLeftImage = new Image();
+playerLeftImage.src = "assets/img/playerLeft.png";
+
+const playerRightImage = new Image();
+playerRightImage.src = "assets/img/playerRight.png";
+
+const player = new Sprite({
+  position: {
+    x: canvas.width / 2 - 192 / 4 / 2,
+    y: canvas.height / 2 - 68 / 2,
+  },
+  image: playerDownImage,
+  frames: {
+    max: 4,
+  },
+  sprites: {
+    up: playerUpImage,
+    left: playerLeftImage,
+    right: playerRightImage,
+    down: playerDownImage,
+  },
+});
+
+const background = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: image,
+});
+const foreground = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: foregroundImage,
+});
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+const movables = [background, ...boundaries, foreground, ...battleZones];
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+    rectangle1.position.y <= rectangle2.position.y + (rectangle2.height - 48) &&
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+  );
+}
+const battle = {
+  initiated: false,
+};
+function animate() {
+  const animationId = window.requestAnimationFrame(animate);
+
+  background.draw();
+
+  boundaries.forEach((boundary) => {
+    boundary.draw();
+  });
+  battleZones.forEach((battleZone) => {
+    battleZone.draw();
+  });
+  player.draw();
+  foreground.draw();
+
+  let moving = true;
+  player.moving = false;
+
+  // console.log(animationId);
+  if (battle.initiated) return;
+  //activate battle
+  if (keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
+    for (let i = 0; i < battleZones.length; i++) {
+      const battleZone = battleZones[i];
+      const overlappingArea =
+        (Math.min(
+          player.position.x + player.width,
+          battleZone.position.x + battleZone.width
+        ) -
+          Math.max(player.position.x, battleZone.position.x)) *
+        (Math.min(
+          player.position.y + player.width,
+          battleZone.position.y + battleZone.height
+        ) -
+          Math.max(player.position.y, battleZone.position.y));
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: battleZone,
+        }) &&
+        overlappingArea > (player.width * player.height) / 2 &&
+        Math.random() < 0.1
+      ) {
+        battle.initiated = true;
+        gsap.to("#overlappingDiv", {
+          opacity: 0,
+          repeat: 3,
+          yoyo: true,
+          duration: 0.5,
+          onComplete() {
+            gsap.to("#overlappingDiv", {
+              opacity: 0,
+              duration: 0.4,
+            });
+
+            // actiavate new animation loop
+
+            // deactivate  current animation loop
+            window.cancelAnimationFrame(animationId);
+          },
+        });
+
+        break;
+      }
+    }
+  }
+  //moving
+  const movingSpeed = 3;
+  if (keys.w.pressed && lastkey === "w") {
+    player.moving = true;
+    player.image = player.sprites.up;
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x,
+              y: boundary.position.y + movingSpeed,
+            },
+          },
+        })
+      ) {
+        moving = false;
+        break;
+      }
+    }
+
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.y += movingSpeed;
+      });
+  } else if (keys.a.pressed && lastkey === "a") {
+    player.moving = true;
+    player.image = player.sprites.left;
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x + movingSpeed,
+              y: boundary.position.y,
+            },
+          },
+        })
+      ) {
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.x += movingSpeed;
+      });
+  } else if (keys.s.pressed && lastkey === "s") {
+    player.moving = true;
+    player.image = player.sprites.down;
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x,
+              y: boundary.position.y - movingSpeed,
+            },
+          },
+        })
+      ) {
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.y -= movingSpeed;
+      });
+  } else if (keys.d.pressed && lastkey === "d") {
+    player.moving = true;
+    player.image = player.sprites.right;
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x - movingSpeed,
+              y: boundary.position.y,
+            },
+          },
+        })
+      ) {
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.x -= movingSpeed;
+        console.log(movingSpeed);
+      });
+  }
+}
+
+window.requestAnimationFrame(animate);
+let lastkey = " ";
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "w":
+      keys.w.pressed = true;
+      lastkey = "w";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      lastkey = "a";
+      break;
+    case "s":
+      keys.s.pressed = true;
+      lastkey = "s";
+      break;
+    case "d":
+      keys.d.pressed = true;
+      lastkey = "d";
+      break;
+  }
+});
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "w":
+      keys.w.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "s":
+      keys.s.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+  }
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+dit is de read me van me javascript game
+
+zelf toegevoegt:
+
+- snelheid in een variable gezet om makkelijk aan te kunnen passen
+- de caracter kan door de top van een collision lopen zo dat het realisischer lijkt met aleen de voetten die niet vedder kunnen lopen
